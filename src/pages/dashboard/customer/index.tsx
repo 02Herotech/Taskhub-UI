3a05dcc34d0702c6d24089ab985b76153273a751
Saheed <saheed.abd.ajayi@gmail.com>
2023-08-27 20:37:40 +0100

    customer folder added

---
+++ src/pages/dashboard/customer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DashboardLayout from "../../../../components/dashboardLayout";
+
+function Index() {
+    return (
+        <DashboardLayout>
+            <div></div>
+        </DashboardLayout>
+    );
+}
+
+export default Index;
