10ad377b2a436539191f1de3020a311f7b996868
Saheed <saheed.abd.ajayi@gmail.com>
2023-09-07 09:41:34 +0100

    customer dashboard progressed

--- components/dashboardLayout/index.tsx
+++ components/dashboardLayout/index.tsx
@@ -6,10 +6,11 @@ import {RxDashboard} from "react-icons/rx";
 import {IoPersonOutline} from "react-icons/io5";
 import {IoClipboardOutline} from "react-icons/io5";
 import {CgBox} from "react-icons/cg";
-import {FiHelpCircle} from "react-icons/fi";
-import { FiLogOut } from "react-icons/fi";
+import {FiHelpCircle, FiMessageCircle} from "react-icons/fi";
+import {FiLogOut} from "react-icons/fi";
 import Image from 'next/image';
 import portrait from "./../../public/dashboardAssets/portrait.jpg";
+import taskHub from "./../../public/dashboardAssets/TASK.png"
 
 interface IProps {
     children: ReactNode
@@ -21,15 +22,19 @@ function DashboardLayout(props: IProps) {
         <>
             <div className={styles.dashboardLayout}>
                 <div className={styles.topBar}>
-                    <div>
-                        <h3 className={styles.name}>
-                            Task<span>HUB</span>
-                        </h3>
+                    <div className={styles.topLogo}>
+                        <Image src={taskHub} className={styles.firstLogo} alt="task-hub"/>
+                        <h3 className={styles.name}> TaskHUB </h3>
                     </div>
                     <div className={styles.userInfo}>
-                        <div className={styles.notificationIndicator}>
+                        <div className={styles.notificationDiv}>
                             <IoIosNotificationsOutline className={styles.notification}/>
                         </div>
+                        <div className={styles.messageDiv}>
+                            <FiMessageCircle className={styles.message}/>
+                            <span className={styles.messageCount}>5</span>
+                        </div>
+
                         <div className={styles.userDetails}>
                             <p className={styles.userName}>Dotun Atom</p>
                             <p className={styles.role}>Customer</p>
@@ -38,23 +43,73 @@ function DashboardLayout(props: IProps) {
                         <RiArrowDropDownLine className={styles.dropdown}/>
                     </div>
                 </div>
-                <div className={styles.bottom}>
+                <div className={styles.sidebarDiv}>
                     <div className={styles.sideBar}>
                         <div>
                             <ol>
-                                <li><RxDashboard size={20}/> Dashboard</li>
-                                <li><IoPersonOutline size={20}/> Profile</li>
-                                <li><IoClipboardOutline size={20}/> Post a Request</li>
-                                <li><CgBox size={20}/> Billing and Payments</li>
-                                <li><FiHelpCircle size={20}/> Help and Support</li>
+                                <button className={styles.sidebarButton}>
+                                    <RxDashboard size={20}/> Dashboard
+                                </button>
+                                <button className={styles.sidebarButton}>
+                                    <IoPersonOutline size={20}/> Profile
+                                </button>
+                                <button className={styles.sidebarButton}>
+                                    <IoClipboardOutline size={20}/> Post a Request
+                                </button>
+                                <button className={styles.sidebarButton}>
+                                    <CgBox size={20}/> Billing and Payments
+                                </button>
+                                <button className={styles.sidebarButton}>
+                                    <FiHelpCircle size={20}/> Help and Support
+                                </button>
                             </ol>
+
                         </div>
                         <button className={styles.logoutButton}>
-                            <FiLogOut size={20} />
+                            <FiLogOut size={20}/>
                             Logout
                         </button>
                     </div>
-                    <div>{props.children}</div>
+                    <div className={styles.content}>{props.children}</div>
+                </div>
+                <div className={styles.footer}>
+                    <div className={styles.footerColumn1}>
+                        <div className={styles.secondLogo}>
+                            <Image src={taskHub} alt="task-hub"/>
+                            <h3>TaskHub</h3>
+                        </div>
+                        <p>
+                            Online platform that connects service providers with customers who are seeking various services.
+                            The platform offers a wide range of services.
+                        </p>
+                        <p>
+                            2023 TaskHub. All Rights Reserved.
+                        </p>
+                    </div>
+                    <div className={styles.footerColumn2}>
+                        <p>Company</p>
+                        <p>About Us</p>
+                        <p>Companies</p>
+                        <p>Pricing</p>
+                    </div>
+                    <div className={styles.footerColumn3}>
+                        <p>Services</p>
+                        <p>Automotive Services</p>
+                        <p>Health & Fitness</p>
+                        <p>Real Estate Services</p>
+                        <p>Delivery & Logistics</p>
+                        <p>Art & Creativity</p>
+                        <p>Travel & Adventure</p>
+                        <p>Childcare & Babysitting</p>
+                        <p>Education & Tutoring</p>
+                    </div>
+                    <div className={styles.footerColumn4}>
+                        <p>Other</p>
+                        <p>FAQs</p>
+                        <p>Contact Us</p>
+                        <p>Terms and Conditions</p>
+                        <p>Privacy</p>
+                    </div>
                 </div>
             </div>
         </>
--- src/pages/dashboard/customer/index.tsx
+++ src/pages/dashboard/customer/index.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import DashboardLayout from "../../../../components/dashboardLayout";
+import customer from "../../../../public/dashboardAssets/portrait.jpg";
+import styles from "../customer/styles.module.scss"
+import Image from 'next/image';
+import {GoVerified} from "react-icons/go";
 
-function Index() {
+interface IProps {
+
+}
+
+function Index(props: IProps) {
     return (
         <DashboardLayout>
-            <div></div>
+            <div>
+                <div>
+                    <div>
+                        <Image src={customer} className={styles.customerImage} alt="customer-image"/>
+                        <div>
+                            <p>Dotun A.</p>
+                            <GoVerified/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </DashboardLayout>
     );
 }
