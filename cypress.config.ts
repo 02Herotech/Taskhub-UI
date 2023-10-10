932fb59faa1a7864dc1eb12de5493ab69e3f7268
O2hero tech <123876597+02Herotech@users.noreply.github.com>
2023-10-11 00:13:39 +0100

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,11 @@
 export default {
   e2e: {
-    setupNodeEvents(on, config) {
+  setupNodeEvents(on: functionType, config: ConfigType) {
       // implement node event listeners here
     },
   },
+};
+type functionType = (/* Specify the parameter and return types for 'on' */ any) => any;
+type ConfigType = {
+  // Define the type for 'config' here
 };
