ad23f7e6460c2dad502a809e6063cd36872a9277
Olamilekan Akanni <cyprusakanni@gmail.com>
2023-11-15 18:05:56 +0100

    signup confirmation modified

--- src/pages/auth/signup-confirmation.tsx
+++ src/pages/auth/signup-confirmation.tsx
@@ -22,7 +22,7 @@ const signupConfirmation = () => {
                     <Image src={welcome} width={166} height={166} alt='' />
                 </div>
                 <p>Before we get started, please use the link in the mail sent to you to verify you email</p>
-                {/* <Link href='https://mail.google.com/mail/u/0/#inbox' target='_blank' className={`text-purpleBase  underline hover:text-md`}>Link to my mail</Link > */}
+                <Link href='https://mail.google.com/mail/u/0/#inbox' target='_blank' className={`text-purpleBase  underline hover:text-md`}>Link to my mail</Link >
 
             </div>
         </div>
