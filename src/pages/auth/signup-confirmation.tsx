f11cbc4b28d8e5e431794405f73ffa07b7379108
Olamilekan Akanni <cyprusakanni@gmail.com>
2023-11-15 22:41:40 +0100

    Email verification fixed

--- src/pages/auth/signup-confirmation.tsx
+++ src/pages/auth/signup-confirmation.tsx
@@ -11,19 +11,21 @@ const signupConfirmation = () => {
     return (
 
         <div className={`m-auto`}>
-            <div className={`w-full p-10 flex drop-shadow-md bg-white h-[80px]`}>
-                <Link href='/' className={`flex space-x-3 items-center`}>
-                    <Image src={logoImg} width={61} height={55} alt='' className={`mt-[-10px]`} />
-                    <h4 className={`text-lg font-extrabold `}>TaskHub</h4>
-                </Link>
+           <div className={`p-5 flex h-[80px] drop-shadow-md fixed z-50 w-full bg-white font-extrabold justify-center`}>
+                <div className='w-[80em]'>
+                    <Link href='/' className={`flex space-x-3 items-center`}>
+                        <Image src={logoImg} width={50} height={40} alt='' className={`mt-[-10px]`} />
+                        <h4 className={`text-sm font-extrabold `}>TaskHub</h4>
+                    </Link>
+                </div>
             </div>
-            <div className={`space-y-3 flex h-full flex-col items-center justify-center m-auto py-20`}>
+
+            <div className={`flex h-full flex-col items-center justify-center min-h-screen pt-20`}>
                 <div className={`w-[166px] h-[166px]`}>
                     <Image src={welcome} width={166} height={166} alt='' />
                 </div>
-                <p>Before we get started, please use the link in the mail sent to you to verify you email</p>
-                <Link href='https://mail.google.com/mail/u/0/#inbox' target='_blank' className={`text-purpleBase  underline hover:text-md`}>Link to my mail</Link >
-
+                <p className='text-center mt-10'>Before we get started, please use the link <br /> in the mail sent to you to verify your email</p>
+                {/* <Link href='https://mail.google.com/mail/u/0/#inbox' target='_blank' className={`text-purpleBase  underline hover:text-md`}>Link to my mail</Link > */}
             </div>
         </div>
 
